fc4a921ab267a527a0d8b419f439d615ddbb6482
Robert Thiel <thielbert7@gmail.com>
2019-08-31 23:20:27 -0600

    Added ship and circuit along with music player.  Ship now flies around circuit in a circle repeatedly while music player loops.  Set initial height for circuit and ship.  Set throw controls (how far a joystick moves over / how fast the throw of a keyboard adjusts the ship along the x axis according to frame rate)

---
+++ Assets/MusicPlayerSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginLevel : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+		Invoke("BeginGame", 2f);
+	}
+
+	void BeginGame() {
+		SceneManager.LoadScene(1);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+	[Tooltip("In m/s^-1")][SerializeField] float xSpeed = 4f;
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+		float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+		print(xOffsetThisFrame);
+	}
+}
