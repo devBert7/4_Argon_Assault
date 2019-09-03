f7d9218d9d1d2138801eaa818cd3dc45219416a1
Robert Thiel <thielbert7@gmail.com>
2019-09-03 08:00:44 -0600

    Cleaning up - rearranged files and fixed scripts, including adding a scene loader script and removing the scene loading ability from the music player and placing it on its own empty object.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BeginLevel : MonoBehaviour {
+public class SceneLoader : MonoBehaviour {
 	// Start is called before the first frame update
 	void Start() {
-		Invoke("BeginGame", 2f);
+		Invoke("LoadFirstScene", 2f);
 	}
 
-	void BeginGame() {
+	void LoadFirstScene() {
 		SceneManager.LoadScene(1);
-	}
-
-	// Update is called once per frame
-	void Update() {
-		
 	}
 }
